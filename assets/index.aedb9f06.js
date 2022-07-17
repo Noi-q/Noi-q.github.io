bc9790d7b04f912e6cdb50bec820ded146246fe6
Noi-q <noi.vcal@gmail.com>
2022-07-17 14:14:50 +0800

    Initial commit

--- assets/index.aedb9f06.js
+++ assets/index.aedb9f06.js
@@ -1,4 +1,4 @@
-import{d as s,r as t,a as i,o as d,c as a,b as c,e,f as p,F as l}from"./index.8c2a27fe.js";const m=`## \u89E3\u51B3KDE\u684C\u9762\u73AF\u5883\u52A0\u8F7DDiscover\u6162\u548C\u52A0\u8F7D\u5931\u8D25\u7684\u95EE\u9898
+import{d as s,r as t,a as i,o as d,c as a,b as c,e,f as p,F as l}from"./index.7fb736b8.js";const m=`## \u89E3\u51B3KDE\u684C\u9762\u73AF\u5883\u52A0\u8F7DDiscover\u6162\u548C\u52A0\u8F7D\u5931\u8D25\u7684\u95EE\u9898
 
 ### \u62A5\u9519\u65E5\u5FD7
 \`\u65E0\u6CD5\u4ECE\u6587\u4EF6 https://autoconfig.kde.org/ocs/providers.xml \u4E2D\u52A0\u8F7D\u5185\u5BB9\u63D0\u4F9B\u8005\u7684\u4FE1\u606F\`
@@ -33,4 +33,4 @@ sudo vim /etc/xdg/providers.xml
 
 #### \u89E3\u51B3\u529E\u6CD53:
 
-\`\u5C06\u4F60\u7684\u754C\u9762\u8BED\u8A00\u6539\u4E3AEnglish,\u540C\u65F6Fotmats\u9ED8\u8BA4\u4E3Adefault.UTF-8\u5C31\u884C\``,v=e("hr",null,null,-1),h=s({__name:"index",setup(u){const n=t(m);return(k,_)=>{const o=i("v-md-preview");return d(),a(l,null,c(3,r=>e("div",{key:r},[p(o,{text:n.value},null,8,["text"]),v])),64)}}});export{h as default};
+\`\u5C06\u4F60\u7684\u754C\u9762\u8BED\u8A00\u6539\u4E3AEnglish,\u540C\u65F6Formats\u9ED8\u8BA4\u4E3Adefault.UTF-8\u5C31\u884C\``,v=e("hr",null,null,-1),h=s({__name:"index",setup(u){const n=t(m);return(k,_)=>{const o=i("v-md-preview");return d(),a(l,null,c(3,r=>e("div",{key:r},[p(o,{text:n.value},null,8,["text"]),v])),64)}}});export{h as default};
---
+++ assets/index.c67d928a.js
@@ -0,0 +1,8 @@
+import{d as s,r as n,a as o,o as i,g as r}from"./index.7fb736b8.js";const c=`<!-- # About -->
+
+<div style="display:flex;justify-content:center;">
+  <img style="border-radius:50%;width:100px;height:100px;" src="https://q2.qlogo.cn/headimg_dl?dst_uin=1044550927&spec=100">
+</div>
+<p style="font-size:30px;">This is My Blog</p>
+<!-- <img height="137px" src="https://github-readme-stats.vercel.app/api?username=Noi-q&hide_title=true&hide_border=true&show_icons=trueline_height=21&text_color=000&icon_color=000&bg_color=0,ea6161,ffc64d,fffc4d,52fa5a&theme=graywhite" /> -->
+<!-- <img src="https://img.shields.io/badge/Vue--cli-4.5.15-green"> -->`,h=s({__name:"index",setup(a){const e=n(c);return(d,p)=>{const t=o("v-md-preview");return i(),r(t,{text:e.value},null,8,["text"])}}});export{h as default};
