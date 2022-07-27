ba78605f0852cb9d68111158291795711ec2c09c
Noi-q <noi.vcal@gmail.com>
2022-07-27 16:07:06 +0800

    Initial commit

--- assets/NotFoundError.8ea16fe3.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./index.aaabeeb5.js";const o={};function t(c,e){return"404"}const _=r(o,[["render",t]]);export{_ as default};
---
+++ assets/index.7df9e546.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.519e6828.js";const r={};function c(n,t){return"Null"}const o=e(r,[["render",c]]);export{o as default};
